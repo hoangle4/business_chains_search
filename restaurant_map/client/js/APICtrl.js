2a21b923f1b3c31864ca74c220def4898c83ab66
Hoang Le <hoang.le4@pcc.edu>
2019-09-13 14:55:32 -0700

    commit files

--- restaurant_map/client/js/APICtrl.js
+++ restaurant_map/client/js/APICtrl.js
@@ -1,23 +1,29 @@
 const APICtrl = (() => {
-  let query;
-  let location;
-  let radius;
+	let query = [ { id: 1, name: 'Pho', active: true } ];
+	let location;
+	let radius;
 
-  const searchResults = async () => {
-    const result = await fetch(
-      `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}&location=${location}&radius=${radius}&key=${GoogleMapKey}`
-    );
-    console.log(result);
-  };
+	const searchResults = async () => {
+		const result = await fetch(
+			`https://maps.googleapis.com/maps/api/place/textsearch/json?query=Starbucks&location=${location}&radius=${radius}&key=${GoogleMapKey}`
+		);
+		console.log(result);
+	};
 
-  return {
-    setData: (q, l, r) => {
-      query = q;
-      location = l;
-      radius = r;
-    },
-    getSearchResults: () => {
-      searchResults();
-    }
-  };
+	return {
+		setLocation: (lat, lng, r) => {
+			location = [ lat, lng ];
+			radius = r;
+			console.log(location, radius);
+		},
+		setQuery: (q) => {
+			query.push({ id: query.length + 1, name: q, active: true });
+		},
+		getSearchResults: () => {
+			searchResults();
+		},
+		getQuery: () => {
+			return query;
+		}
+	};
 })();
--- restaurant_map/client/js/APP.js
+++ restaurant_map/client/js/APP.js
@@ -1,29 +1,27 @@
 const APP = ((MapCtrl, UICtrl, APICtrl) => {
-  const Map_Btn = document.getElementById("map_now_btn");
-  const Trumbotron_Container = document.getElementById("trumbotron_container");
-  const addPlaceBtn = document.getElementById("add_place_btn");
-  const placeNameInput = document.getElementById("placename_input");
-  //----------------------------------------------------------------
-  const initMap = e => {
-    e.preventDefault();
-    MapCtrl.initMap();
-    Trumbotron_Container.remove();
-  };
+	const Map_Btn = document.getElementById('map_now_btn');
+	const Trumbotron_Container = document.getElementById('trumbotron_container');
+	const addPlaceBtn = document.getElementById('add_place_btn');
+	const placeNameInput = document.getElementById('placename_input');
+	//----------------------------------------------------------------
+	const initMap = (e) => {
+		e.preventDefault();
+		MapCtrl.initMap();
+		Trumbotron_Container.remove();
+	};
 
-  const eventHandler = () => {
-    addPlaceBtn.addEventListener("click", handleAddPlace);
-    placeNameInput.addEventListener(
-      "keyup",
-      e => e.keyCode === 13 && handleAddPlace()
-    );
-  };
+	const eventHandler = () => {
+		addPlaceBtn.addEventListener('click', handleAddPlace);
+		placeNameInput.addEventListener('keyup', (e) => e.keyCode === 13 && handleAddPlace());
+	};
 
-  const handleAddPlace = e => {
-    e && e.target.tagName === "BUTTON" && e.preventDefault();
-    placeNameInput.value !== "" && UICtrl.addPlace(placeNameInput.value);
-    placeNameInput.value = "";
-  };
+	const handleAddPlace = (e) => {
+		e && e.target.tagName === 'BUTTON' && e.preventDefault();
+		placeNameInput.value !== '' && UICtrl.addPlace(placeNameInput.value);
+		APICtrl.setQuery(placeNameInput.value);
+		placeNameInput.value = '';
+	};
 
-  Map_Btn.addEventListener("click", initMap);
-  document.addEventListener("DOMContentLoaded", eventHandler);
+	Map_Btn.addEventListener('click', initMap);
+	document.addEventListener('DOMContentLoaded', eventHandler);
 })(MapCtrl, UICtrl, APICtrl);
