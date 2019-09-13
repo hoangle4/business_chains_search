a8c9fef610d4b5d4b2ec62e5304816df70635ceb
Hoang Le <hoang.le4@pcc.edu>
2019-09-13 15:48:36 -0700

    commit server files

--- restaurant_map/client/js/APICtrl.js
+++ restaurant_map/client/js/APICtrl.js
@@ -4,10 +4,19 @@ const APICtrl = (() => {
 	let radius;
 
 	const searchResults = async () => {
-		const result = await fetch(
-			`https://maps.googleapis.com/maps/api/place/textsearch/json?query=Starbucks&location=${location}&radius=${radius}&key=${GoogleMapKey}`
+		const xhttp = new XMLHttpRequest();
+		xhttp.open(
+			'GET',
+			`https://maps.googleapis.com/maps/api/place/textsearch/json?query=Starbucks&location=${location}&radius=${radius}&key=${GoogleMapKey}`,
+			true
 		);
-		console.log(result);
+
+		xhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				console.log(xhttp);
+			}
+		};
+		xhttp.send();
 	};
 
 	return {
