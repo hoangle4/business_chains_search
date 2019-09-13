b667157156110e0fdb9313065d4acd42e0a6df05
Hoang Le <hoang.le4@pcc.edu>
2019-09-13 15:50:02 -0700

    commit server files

--- restaurant_map/server/server.js
+++ restaurant_map/server/server.js
@@ -3,4 +3,8 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+app.post('/getPlaces', (req, resp) => {
+	// getPlaces here
+});
+
 app.listen(PORT, () => console.table({ SERVER: 'RUNNING', PORT }));
