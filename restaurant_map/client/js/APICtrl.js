5014f76b4526be819cde72fec5f4ec3319f694e2
hoangle4 <hoang.le4@pcc.edu>
2019-09-13 22:23:03 -0700

    commit files

--- restaurant_map/client/js/APICtrl.js
+++ restaurant_map/client/js/APICtrl.js
@@ -1,38 +1,38 @@
 const APICtrl = (() => {
-	let query = [ { id: 1, name: 'Pho', active: true } ];
-	let location;
-	let radius;
+  let query = [{ id: 1, name: "Pho", active: true }];
+  let location;
+  let radius;
 
-	const searchResults = async () => {
-		const xhttp = new XMLHttpRequest();
-		xhttp.open(
-			'GET',
-			`https://maps.googleapis.com/maps/api/place/textsearch/json?query=Starbucks&location=${location}&radius=${radius}&key=${GoogleMapKey}`,
-			true
-		);
+  const searchResults = async () => {
+    const xhttp = new XMLHttpRequest();
+    xhttp.open(
+      "GET",
+      `http://localhost:3001/getPlaces?location=${location}&radius=${radius}`,
+      true
+    );
 
-		xhttp.onreadystatechange = function() {
-			if (this.readyState == 4 && this.status == 200) {
-				console.log(xhttp);
-			}
-		};
-		xhttp.send();
-	};
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        console.log(JSON.parse(this.response));
+      }
+    };
+    xhttp.send();
+  };
 
-	return {
-		setLocation: (lat, lng, r) => {
-			location = [ lat, lng ];
-			radius = r;
-			console.log(location, radius);
-		},
-		setQuery: (q) => {
-			query.push({ id: query.length + 1, name: q, active: true });
-		},
-		getSearchResults: () => {
-			searchResults();
-		},
-		getQuery: () => {
-			return query;
-		}
-	};
+  return {
+    setLocation: (lat, lng, r) => {
+      location = [lat, lng];
+      radius = r;
+      console.log(location, radius);
+    },
+    setQuery: q => {
+      query.push({ id: query.length + 1, name: q, active: true });
+    },
+    getSearchResults: () => {
+      searchResults();
+    },
+    getQuery: () => {
+      return query;
+    }
+  };
 })();
--- restaurant_map/server.js
+++ restaurant_map/server.js
@@ -1,10 +1,38 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-
+const cors = require("cors");
+const axios = require("axios");
 const PORT = process.env.PORT || 3001;
 
-app.post('/getPlaces', (req, resp) => {
-	// getPlaces here
+app.use(
+  cors({
+    // origin: function(origin, callback) {
+    //   // allow requests with no origin
+    //   // (like mobile apps or curl requests)
+    //   if (!origin) return callback(null, true);
+    //   if (allowedOrigins.indexOf(origin) === -1) {
+    //     var msg =
+    //       "The CORS policy for this site does not " +
+    //       "allow access from the specified Origin.";
+    //     return callback(new Error(msg), false);
+    //   }
+    //   return callback(null, true);
+    // }
+    origin: "*"
+  })
+);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+app.get("/getPlaces", async (req, resp) => {
+  const query = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=Starbucks&location=${req.query.location}&radius=${req.query.radius}&key=${process.env.GOOGLE_MAP_API}&maxResults=200`;
+  const results = await axios.get(query);
+
+  resp.json(results.data.results);
 });
 
-app.listen(PORT, () => console.table({ SERVER: 'RUNNING', PORT }));
+app.listen(PORT, () => console.table({ SERVER: "RUNNING", PORT }));
